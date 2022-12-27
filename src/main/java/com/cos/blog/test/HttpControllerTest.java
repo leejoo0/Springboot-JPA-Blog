c38b10e4bb713972f9761d1ebf5b8dac3d29a318
leejy <leejoo0@bluedigm.com>
2022-12-27 14:33:42 +0900

    lombok과 빌더패턴

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -8,6 +8,25 @@ import org.springframework.web.bind.annotation.*;
 // 사용자가 요청 ->응답(Data)
 @RestController
 public class HttpControllerTest {
+
+    private static final String TAG = "HttpControllerTest";
+
+    /*롬복 빌더 장점. 빌더패턴을 만들어야하는데 직접 만들어준다.
+    * 빌더패턴 장점: 생성자 입력 순서를 지키지 않아도 된다.
+    * 필드에 어떤 값이 들어가야하는지 몰라도 가능*/
+
+    @GetMapping("/http/lombok")
+    public String lombokTest() {
+      //  Member m = new Member(1,"sarr","1234","email");
+        Member m = Member.builder().username("sarr").password("1234").email("ssar@nate.com").build();
+        System.out.println(TAG + "getter" + m.getUsername());
+        m.setUsername("cos");
+        System.out.println(TAG + "setter" + m.getUsername());
+
+
+        return "lombok Test 완료";
+    }
+
     //인터넷 브라우저 요청은 무조건 GET 요청만 할 수 있다.
     //http://localhost:8080/http/get (select)
     //http://localhost:8080/http/get?id=1&username=lee
--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,49 +1,28 @@
 package com.cos.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+  // 전체 생성자
+@NoArgsConstructor   // 빈 생성자
+
 public class Member {
 
     //java에서 (객체지향) 변수는 private로 만든다. 변수의 상태는 메서드에 의해 변경이 되게 설계해야한다.
+//   final 붙여서 불변성 유지. 수정 안되게 막음
     private int id;
     private String username;
     private String password;
     private String email;
 
+    @Builder   //빌더 패턴. 사용은 Member.builer 로 사용하고, id 증가값 만들기 등 가능
     public Member(int id, String username, String password, String email) {
         this.id = id;
         this.username = username;
         this.password = password;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
 }
