b3d6b21d66bba56f27c177f89365bdf78c4129e2
leejy <leejoo0@bluedigm.com>
2022-12-27 16:26:40 +0900

    user Entity 생성

---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,34 @@
+package com.cos.blog.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+//ORM -> java(다른언어) Object -> 테이블로 매핑 해주는 기술
+@Entity  //Userclass가 Mysql에 테이블이 생성이 된다.
+public class User {
+
+    @Id //primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // 프로젝트에서 연결 된 데이터베이스 넘버링 전략을 따라간다는 뜻.
+    private int id; // 시퀀스 , auto_increment
+
+    @Column(nullable = false,length = 30) //null 안됨 길이 설정
+    private String username; // 아이디
+
+    @Column(nullable = false,length = 100) // 해쉬 사용해서 비밀번호 암호화
+    private String password;
+
+    @Column(nullable = false,length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role; //Enum 을 쓰는게 좋음 //admin , user , manager(managerrrr)
+    //Enum을 쓰면 도메인 (범위를 설정 할 수 있음)
+
+    @CreationTimestamp //시간이 자동입력 됨.
+    private Timestamp createDate;
+
+
+}
